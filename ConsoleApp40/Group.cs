d89a7eca03a26c79c473bd255f8bc85e253f84bb
unknown <tu7gbzmx6@code.edu.az>
2022-03-17 09:55:58 +0400

    ConsoleApp40 files

---
+++ ConsoleApp40/Group.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp40
+{
+    internal class Group
+    {
+        public string _No;
+        public int _Studentlimit;
+        public string No
+        {
+            get =>this._No;
+            set
+            {
+                if (CheckNo)
+                {
+                    this._No = value;
+                }
+            }
+
+        }
+        public int Studentlimit
+        {
+            get =>this._Studentlimit;
+            set
+            {
+                if (value>=0)
+                {
+                    this._Studentlimit = value;
+                }
+            }
+        }
+        static bool ChecNo(string No)
+        {
+            if (!string.IsNullOrEmpty(No))
+            {
+                if(char.IsUpper(No[0])&& char.IsUpper(No[1])&& No.Length == 3)
+                {
+                    for (int i = 0; i < No.Length; i++)
+                    {
+                        if (char.IsDigit(No[i]))
+                        {
+                            return false;
+                        }
+                        return true;
+                    }
+                    return false;
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp40/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ConsoleApp40
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Eded daxil edin");
+            string counts=Console.ReadLine();
+            int count;
+            Group[] arr=new Group[counts.Length];
+            for (int i=0; i<counts.Length; i++)
+            {
+                string No;
+                string Studentlimit;
+                int studentlimit;
+                Console.WriteLine(i+1+"Qrupun nomresini daxil edin");
+                No = Console.ReadLine();
+
+                Console.WriteLine(i + 1 + "Telebe limitini daxil edin");
+                Studentlimit = Console.ReadLine();
+                while (!int.TryParse(Studentlimit,out Studentlimit))
+                {
+                    Console.WriteLine(i + 1 + "Telebe limitini daxil edin");
+                    Studentlimit = Console.ReadLine();
+                }
+            } 
+        }
+        static bool CheckNo(string No)
+        {
+            if (!string.IsNullOrEmpty(No)&& No.Length==3 && char.IsUpper(No[0]) && char.IsUpper(No[1]))
+            {
+                for (int i = 0; i < No.Length; i++)
+                {
+                    if (char.IsDigit(No[i]))
+                    {
+                        return false;
+
+                    } 
+                }
+                return true;
+            }
+            return false;
+        }
+    }
+}
